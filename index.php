7bfaf7994c39d625e6756e0034ff5e76d2b9644d
Vk Social <143041574+vksocial@users.noreply.github.com>
2023-08-26 18:43:55 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html> 
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <title>Ace - The High Performance Code Editor for the Web</title>
+        <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/twitter-bootstrap/2.2.2/css/bootstrap.no-icons.min.css" />
+        <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/3.0/css/font-awesome.css" />
+        <link href="./api/resources/csses/ace_api.css" rel="stylesheet" type="text/css" />
+        <link href="./doc/site/style.css" rel="stylesheet" type="text/css" />
+        <link href="./doc/site/images/favicon.ico" rel="icon" type="image/x-icon" />
